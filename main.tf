f1ec819c7ff4b7dad7c93a1cee9014a91852c93c
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-14 13:34:03 +0530

    frontend jenkins

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 module "network" {
-  source = "https://github.com/pavanikakarlapudi/tf-module-vpc.git"
-  env    = var.env
+  source         = "github.com/pavanikakarlapudi/tf-module-vpc"
+  env            = var.env
+  default_vpc_id = var.default_vpc_id
 
-  for_each   = var.vpc
-  cidr_block = each.value.cidr_block
+  for_each     = var.vpc
+  cidr_block   = each.value.cidr_block
+  subnets_cidr = each.value.subnets_cidr
 }
