dd4c9ae3c5665dcbddae6caa2af1c700aa9e4a51
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-14 12:50:30 +0530

    frontend jenkins

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${var.env}"
-  }
+
+module "network" {
+  source = "github.com/pavanikakarlapudi/tf-module-vpc"
+  env    = var.env
+
+  for_each   = var.vpc
+  cidr_block = each.value.cidr_block
 }
