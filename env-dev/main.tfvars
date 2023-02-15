01ba3ffb8584072c98cac962ac8923e34a23f4c5
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-15 21:18:07 +0530

    frontend jenkins

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,2 +1,8 @@
 env = "dev"
 
+vpc = {
+  main = {
+    cidr_block = "10.0.0.0/16"
+  }
+}
+
--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
 module "network" {
   source = "github.com/pavanikakarlapudi/tf-module-vpc"
+  for_each = var.vpc
+  cidr_block = each.value.cidr_block
 }
