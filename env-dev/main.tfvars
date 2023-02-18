1eaedbd84da19636d8fd48377945f30aace9bcea
pavani <pavanikakarlapudi1990@gmail.com>
2023-02-18 21:16:03 +0530

    frontend jenkins

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -1,12 +1,23 @@
-env = "dev"
+env            = "dev"
 default_vpc_id = "vpc-0a7931ca7fa7ad5a2"
 
 vpc = {
   main = {
     cidr_block = "10.0.0.0/16"
-    public_subnets_cidr = ["10.0.0.0/24", "10.0.1.0/24"]
-    private_subnets_cidr = ["10.0.3.0/24", "10.0.4.0/24"]
-    availability_zones = ["us-east-1a", "us-east-1b"]
   }
 }
 
+subnets = {
+  public = {
+
+  }
+  web = {
+
+  }
+  app = {
+
+  }
+  db = {
+
+  }
+}
--- main.tf
+++ main.tf
@@ -1,11 +1,8 @@
 module "network" {
-  source = "github.com/pavanikakarlapudi/tf-module-vpc"
-  env = var.env
+  source         = "github.com/pavanikakarlapudi/tf-module-vpc"
+  env            = var.env
   default_vpc_id = var.default_vpc_id
 
-  for_each = var.vpc
+  for_each   = var.vpc
   cidr_block = each.value.cidr_block
-  public_subnets_cidr = each.value.public_subnets_cidr
-  private_subnets_cidr = each.value.private_subnets_cidr
-  availability_zones = each.value.availability_zones
 }
